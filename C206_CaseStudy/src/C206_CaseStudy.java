913768cd47cacbce07e17078007aa6032b7c8b49
19019456 <19019456@DESKTOP-KPT753S>
2020-08-18 15:27:58 +0800

    The corrected code, sorry

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -4,7 +4,11 @@ public class C206_CaseStudy {
 
 	private static final int QUIT = 6;
 	private static final int MANAGE_PACKAGE = 1;
-
+	private static final int MANAGE_CUSTOMER = 2;
+	private static final int MANAGE_QUOTATION = 3;
+	private static final int MANAGE_REQUEST = 4;
+	private static final int MANAGE_APPOINTMENT = 5;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Package> packageList = new ArrayList<Package>();
@@ -14,10 +18,23 @@ public class C206_CaseStudy {
 			C206_CaseStudy.menu();
 			option = Helper.readInt("Choose option> ");
 			if (option == MANAGE_PACKAGE) {
+				inputType();
+				if (option == 1) {
+					C206_CaseStudy.inputPackage();
+					
+				} else if (option == 2) {
+					C206_CaseStudy.viewAllPackages(packageList);
+				} 
 				
-			} else if (option == 2) {
 				
-			} else if (option == 3) {
+				
+			} else if (option == MANAGE_CUSTOMER) {
+				
+			} else if (option == MANAGE_QUOTATION) {
+				
+			} else if (option == MANAGE_REQUEST) {
+				
+			} else if (option == MANAGE_APPOINTMENT) {
 				
 			}
 			
@@ -40,6 +57,14 @@ public class C206_CaseStudy {
 		
 	}
 	
+	private static void inputType() {
+		System.out.println("INPUT TYPES");
+		System.out.println("1. Add items");
+		System.out.println("2. View items");
+		System.out.println("3. Delete items");
+	}
+	
+	
 	public static void setHeader(String header) {
 		Helper.line(80, "-");
 		System.out.println(header);
@@ -51,7 +76,7 @@ public class C206_CaseStudy {
 	public static Package inputPackage() {
 		String pcode = Helper.readString("Enter package code> ");
 		String description = Helper.readString("Enter package description> ");
-		String startDate = Helper.readString("Enter package start date");
+		String startDate = Helper.readString("Enter package start date> ");
 		String endDate = Helper.readString("Enter package end date> ");
 		double packageAmount = Helper.readDouble("Enter package amount> ");
 		boolean availablePackage = Helper.readBoolean("Is the package available? > ");
@@ -61,9 +86,35 @@ public class C206_CaseStudy {
 	}
 	
 	public static void addPackage(ArrayList<Package> packageList, Package p) {
+		inputPackage();
 		packageList.add(p);
 		System.out.println("Package added!");
 	}
+	
+	public static String retrieveAllPackages(ArrayList<Package> packageList) {
+		String output = "";
+		// write your code here
+		for (int i = 0; i < packageList.size(); i++) {
+			output += String.format("%-20s\n", packageList.get(i).toString());
+		}
+		return output;
+	}
+	
+	
+	public static void viewAllPackages(ArrayList<Package> packageList) {
+		C206_CaseStudy.setHeader("VIEW ALL PACKAGES");
+		String output = String.format("%-10s %-10s %-10s %-10s %-10s %-10s", 
+				"PACKAGE CODE", "DESCRIPTION", "PACKAGE AMOUNT", "PACKAGE START DATE", "PACKAGE END DATE", "AVAILABILITY");
+		output += retrieveAllPackages(packageList);
+		System.out.println(output);
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
