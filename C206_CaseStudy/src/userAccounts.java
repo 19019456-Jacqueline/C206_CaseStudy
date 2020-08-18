f5d028a7560762f38c9c85e70fdf4e082fc085dc
19020090 <19020090@LAPTOP-13HTSDEK>
2020-08-18 12:03:21 +0800

    Pushing userAccounts

---
+++ C206_CaseStudy/src/userAccounts.java
@@ -0,0 +1,31 @@
+
+public class userAccounts {
+	private String name;
+	private String role;
+	private String email;
+	private String password;
+	private String status;
+	
+	public userAccounts(String name, String role, String email, String password, String status) {
+		this.name = name;
+		this.role = role;
+		this.email = email;
+		this.password = password;
+		this.status = status;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getRole() {
+		return role;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getStatus() {
+		return status;
+	}
+}
