ef86c047a945ec94e6088877d387bca18ec5474f
19020090 <19020090@LAPTOP-13HTSDEK>
2020-08-18 15:03:00 +0800

    Manage Customers ( Add and View ) - Skye

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -8,6 +8,7 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Package> packageList = new ArrayList<Package>();
+		ArrayList<userAccounts> userAccList = new ArrayList<userAccounts>();
 	
 		int option = 0;
 		while (option != QUIT) {
@@ -16,6 +17,21 @@ public class C206_CaseStudy {
 			if (option == 1) {
 				
 			} else if (option == 2) {
+				// MANAGE CUSTOMERS
+				C206_CaseStudy.setHeader("MANAGE CUSTOMERS");
+				System.out.println("1. Add User Accounts");
+				System.out.println("2. View all users");
+				System.out.println("3. Delete users");
+				
+				int userOptions = Helper.readInt("Enter option> ");
+				
+				if (userOptions == 1) {
+					userAccounts ua = inputUserAccounts();
+					C206_CaseStudy.addUserAccounts(userAccList, ua);
+				}
+				else if (userOptions == 2) {
+					C206_CaseStudy.viewUserAccounts(userAccList);
+				}
 				
 			} else if (option == 3) {
 				
@@ -60,7 +76,43 @@ public class C206_CaseStudy {
 		return p;
 	}
 	
+	//Manage users (Skye)
+	//========== Option 1 Add User Accounts =============
+	public static userAccounts inputUserAccounts() {
+		String uName = Helper.readString("Enter name> ");
+		String uRole = Helper.readString("Enter role> ");
+		String uEmail = Helper.readString("Enter email> ");
+		String uPassword = Helper.readString("Enter password> ");
+		String uStatus = Helper.readString("Enter status> ");
+		
+		userAccounts uAcc = new userAccounts(uName, uRole, uEmail, uPassword, uStatus);
+		return uAcc;
+	}
+	public static void addUserAccounts(ArrayList<userAccounts> userAccList, userAccounts uAcc) {
 	
+		userAccList.add(uAcc);
+		System.out.println("User Account added!");
+	}
 	
-
+	//======= Option 2 View all users ==============
+	public static String retrieveAllUsers(ArrayList<userAccounts> userAccList) {
+		String output = "";
+		
+		for (int i = 0; i < userAccList.size(); i++) {
+			output += String.format("%-10s %-20s %-10s %-10s %-20s\n", 
+					userAccList.get(i).getName(),
+					userAccList.get(i).getRole(), 
+					userAccList.get(i).getEmail(),
+					userAccList.get(i).getPassword(),
+					userAccList.get(i).getStatus());
+		}
+		return output;
+	}
+	public static void viewUserAccounts(ArrayList<userAccounts> userAccList) {
+		C206_CaseStudy.setHeader("User Accounts");
+		String output = String.format("%-10s %-20s %-10s %-10s %-20s\n", "NAME", "ROLE",
+				"EMAIL", "PASSWORD","STATUS");
+		output += retrieveAllUsers(userAccList);	
+		System.out.println(output);
+	}
 }
